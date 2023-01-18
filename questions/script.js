90891024251dffe87e856528c1c8d3d5edfbdb9a
TecHeReTiC <talex73@mail.ru>
2023-01-18 23:05:21 +0500

    Now all answers are closed if an answer is opened. Questions seem to be ready

--- questions/script.js
+++ questions/script.js
@@ -1,16 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     let toggle_buttons = [...document.querySelectorAll('.toggle')];
     let questions = [...document.querySelectorAll('.questions li')];
+
+    function open(cur_quest, cur_button) {
+        cur_quest.classList.add('active');
+        cur_button.classList.add('fa-minus-square');
+        cur_button.classList.remove('fa-plus-square');
+    }
+
+    function close(cur_quest, cur_button) {
+        cur_quest.classList.remove('active');
+        cur_button.classList.add('fa-plus-square');
+        cur_button.classList.remove('fa-minus-square');
+    }
+
     for (let i = 0; i < toggle_buttons.length; ++i) {
         let cur_button =  toggle_buttons[i];
         cur_button.dataset.quest_number = `${i}`;
         questions[i].dataset.quest_number = `${i}`;
         cur_button.addEventListener('click', () => {
-            console.log(cur_button.dataset.quest_number);
             let cur_quest = questions[Number(cur_button.dataset.quest_number)];
-            cur_quest.classList.toggle('active');
-            cur_button.classList.toggle('fa-plus-square');
-            cur_button.classList.toggle('fa-minus-square');
+            if (!cur_quest.classList.contains('active')) {
+                for (let i = 0; i < toggle_buttons.length; ++i) {
+                    close(questions[i], toggle_buttons[i]);
+                }
+                open(cur_quest, cur_button);
+            } else {
+                close(cur_quest, cur_button);
+            }
+
+
         });
     }
 });
