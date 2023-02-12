22eafaaaec51a9239d7c9d4d60e16481e6614fed
TecHeReTiC <talex73@mail.ru>
2023-02-12 19:47:30 +0500

    Code refactoring and little styles changes

--- countdown/script.js
+++ countdown/script.js
@@ -5,7 +5,8 @@ let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 document.addEventListener('DOMContentLoaded', () => {
 
     let giveAwayEnding = document.querySelector('.giveaway-ending');
-    let [days, hrs, mins, secs] = document.querySelectorAll('.time-left li span');
+    let timeLeft = document.querySelector('.time-left');
+    let [days, hrs, mins, secs] = timeLeft.querySelectorAll('li span');
     console.log(days, hrs, mins, secs);
 
     function setupDate() {
@@ -19,20 +20,34 @@ document.addEventListener('DOMContentLoaded', () => {
         return ending.getTime();
     }
     let ending_time = setupDate();
+    console.log(ending_time);
     let timeTimezoneOffset = -new Date().getTimezoneOffset() * 60 * 1000;
+
+    let formatValue = (value => {
+        return (value < 10 ? `0${value}` : value);
+    });
+
     function updateTimeLeft() {
-        let time_left = new Date(ending_time - Date.now() - timeTimezoneOffset);
-        days.innerText = Math.floor(time_left.getTime() / (24 * 3600 * 1000));
-        if (days.innerText < 10) {
-            days.innerText = `0${days.innerText}`;
+        if (ending_time - Date.now() - timeTimezoneOffset < 0) {
+            clearInterval(updateInt);
+            timeLeft.replaceChildren();
+            let expired = document.createElement('h4');
+            expired.classList.add('expired');
+            expired.innerText = 'Sorry, this giveaway has expired';
+            timeLeft.appendChild(expired);
         }
-        let hours = time_left.getHours(),
+        let time_left = new Date(ending_time - Date.now() - timeTimezoneOffset);
+        console.log(time_left);
+
+        let days = Math.floor(time_left.getTime() / (24 * 3600 * 1000)),
+            hours = time_left.getHours(),
             minutes = time_left.getMinutes(),
             seconds = time_left.getSeconds();
-        hrs.innerText = (hours < 10 ? `0${hours}` : hours);
-        mins.innerText = (minutes < 10 ? `0${minutes}` : minutes);
-        secs.innerText = (seconds < 10 ? `0${seconds}` : seconds);
+        let parts = [days, hours, minutes, seconds];
+        [days, hrs, mins, secs].forEach((item, index) => {
+            item.innerText = formatValue(parts[index]);
+        });
     }
     updateTimeLeft();
-    setInterval(updateTimeLeft, 1000);
+    let updateInt = setInterval(updateTimeLeft, 1000);
 });
