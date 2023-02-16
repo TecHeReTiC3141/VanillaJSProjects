a6dba561a10cebbe7d4277fbd71aac6e8ddf9d82
TecHeReTiC <talex73@mail.ru>
2023-02-16 11:06:19 +0500

    Local storage is in progress

--- grocery_bud/script.js
+++ grocery_bud/script.js
@@ -6,8 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
     let message = document.querySelector('.message');
     let clearBtn = document.querySelector('.clear');
 
-    let idCounter = 0;
-
     const btnContainer = '<div class="btn-container">\n' +
         '                    <button class="edit"><i class="fas fa-edit"></i></button>\n' +
         '                    <button class="delete"><i class="fas fa-trash"></i></button>\n' +
@@ -22,13 +20,13 @@ document.addEventListener('DOMContentLoaded', () => {
         return element;
     }
 
-    function generateNewItem(val) {
+    function generateNewItem(val, iden=Date.now()) {
         let newName = createElement('p', val, 'grocery');
         let item = document.createElement('li');
 
         item.append(newName);
         item.insertAdjacentHTML('beforeend', btnContainer);
-        item.dataset.iden = (++idCounter).toString();
+        item.dataset.iden = iden;
         const editBtn = item.querySelector('.edit');
         editBtn.dataset.iden = item.dataset.iden;
         const deleteBtn = item.querySelector('.delete');
@@ -40,6 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
         deleteBtn.addEventListener('click', () => {
             actionButton.value = 'submit';
             product.value = '';
+            deleteFromLocalStorage(iden);
             deleteBtn.parentElement.parentElement.remove();
             console.log(toBuyList.children);
             if (!toBuyList.hasChildNodes()) {
@@ -50,6 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(item);
         toBuyList.appendChild(item);
         clearBtn.style.display = 'block';
+        addToLocalStorage(iden, val);
     }
 
     function addMessage(value, type) {
@@ -62,30 +62,56 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1000);
     }
 
+    function returnToDefault() {
+        product.value = '';
+        actionButton.value = 'submit';
+        delete actionButton.dataset.curEdit;
+    }
+
     form.addEventListener('submit', e => {
         e.preventDefault();
-        console.log(product.value, actionButton.value);
         if (!product.value) {
             addMessage('please enter value', 'error');
         } else if (actionButton.value === 'submit') {
             generateNewItem(product.value);
             addMessage('new item was added', 'success');
+            returnToDefault();
         } else if (actionButton.value === 'edit') {
-            console.log(actionButton.dataset.curEdit);
             let editedItem = document.querySelector(
                 `.to-buy li[data-iden="${actionButton.dataset.curEdit}"]`);
             editedItem.querySelector('.grocery').innerHTML = product.value;
-            delete actionButton.dataset.curEdit;
+            addToLocalStorage(editedItem.dataset.iden, product.value);
             addMessage('value was changed', 'success');
-            actionButton.value = 'submit';
+            returnToDefault();
         }
-        product.value = '';
     });
-
 
     clearBtn.addEventListener('click', () => {
-        toBuyList.replaceChildren();
+        while (toBuyList.firstChild) {
+            console.log(toBuyList.lastChild);
+            deleteFromLocalStorage(toBuyList.lastChild.dataset.iden);
+            toBuyList.removeChild(toBuyList.lastChild);
+        }
         addMessage('items cleared', 'error');
         clearBtn.style.display = 'none';
+        returnToDefault();
     });
+
+    function restoreItems() {
+        for (let key of Object.keys(localStorage)) {
+            generateNewItem(localStorage.getItem(key), key);
+        }
+    }
+
+    function addToLocalStorage(id, value) {
+        console.log(`${id} with value ${value} was added to LS`)
+        localStorage.setItem(id, value);
+    }
+
+    function deleteFromLocalStorage(id) {
+        console.log(`${id} with value ${localStorage.getItem(id)} was deleted from LS`)
+        localStorage.removeItem(id);
+    }
+
+    restoreItems();
 });
