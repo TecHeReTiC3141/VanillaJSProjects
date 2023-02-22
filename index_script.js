578de15f4fbe6842abfd250ebe0603655e3c4e6a
TecHeReTiC <talex73@mail.ru>
2023-02-23 02:49:03 +0500

    Projects previews have been added

---
+++ index_script.js
@@ -0,0 +1,13 @@
+let images = [
+    'color flipper',
+    'simple counter',
+    'reviews', 'navbar',
+    'sidebar', 'modal',
+    'questions', 'menu',
+    'video', 'scroll',
+    'tabs', 'countdown timer',
+    'lorem ipsum', 'grocery bud',
+    'slider'
+];
+
+
