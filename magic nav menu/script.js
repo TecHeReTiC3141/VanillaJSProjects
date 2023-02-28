3826eb8eafeba14b43ac3630baa1b1d1e9daa0e9
TecHeReTiC <talex73@mail.ru>
2023-02-28 13:46:15 +0500

    Favicons and social links have been added. Light theme seems to be ready

--- magic nav menu/script.js
+++ magic nav menu/script.js
@@ -11,21 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
             e.currentTarget.classList.add('active');
             let targ_rect = e.currentTarget.querySelector('i').getBoundingClientRect();
             let targ_center_x = (targ_rect.left + targ_rect.right) / 2;
-            activeBg.style.left = `${(targ_center_x - (activeBg.clientWidth + 16) / 2 - nav.getBoundingClientRect().left)}px`;
+            activeBg.style.left = `${(targ_center_x - (activeBg.clientWidth + 
+                (content.classList.contains('dark') ? 16 : 0)) / 2 - nav.getBoundingClientRect().left)}px`;
         });
     });
 
-    window.addEventListener('resize', () => {
-        console.log('resized');
+    function set_indicator() {
         for (let nav_el of nav_elements) {
             if (nav_el.classList.contains('active')) {
                 let targ_rect = nav_el.querySelector('i').getBoundingClientRect();
                 let targ_center_x = (targ_rect.left + targ_rect.right) / 2;
-                activeBg.style.left = `${(targ_center_x - (activeBg.clientWidth + 16) / 2 - nav.getBoundingClientRect().left)}px`;
+                activeBg.style.left = `${targ_center_x - (activeBg.clientWidth +
+                    (content.classList.contains('dark') ? 16 : 0)) / 2 - nav.getBoundingClientRect().left}px`;
                 break;
             }
         }
-    });
+    }
+
+    window.addEventListener('resize', set_indicator);
+
 
     let themeToggler = document.querySelector('.theme-toggler');
     let [lightBtn, darkBtn] = themeToggler.querySelectorAll('i');
@@ -37,5 +41,6 @@ document.addEventListener('DOMContentLoaded', () => {
         lightBtn.classList.toggle('fa-regular');
         darkBtn.classList.toggle('fa-solid');
         darkBtn.classList.toggle('fa-regular');
+        set_indicator();
     });
 });
